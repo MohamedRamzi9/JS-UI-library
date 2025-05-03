175466644424e55ec1c136cf2d1ce1d473658745
MohamedRamzi9 <dragunofredeyes@hotmail.com>
2025-05-03 11:42:47 +0100

    commit

--- URL.js
+++ URL.js
@@ -1,21 +1,30 @@
 
 
-// return a the encoded url as a string from a json object
+// returns a the encoded url as a string from a json object
 export function to_string(json) {
 	return new URLSearchParams(json).toString();
 }
 
-// return a json object from an encoded url string
+// returns a json object from an encoded url string
 export function to_json(string) {
 	return Object.fromEntries(new URLSearchParams(string).entries());
 }
 
-// return the pathhname of the current url without the leading slash
-export function get_pathname(url) {
+// returns the pathname of the current url without the leading slash
+export function path() {
 	return location.pathname.replaceAll("/", '');
 }
+// returns the full url of the current page
+export function url() {
+	return location.href;
+}
 
-// set the pathhname of the current url without the leading slash
-export function set_pathname(pathname) {
-	location.pathname = '/' + pathname;
+// sets the pathname of the current url without the leading slash
+export function goto_path(path) {
+	location.pathname = '/' + path;
+}
+
+// sets the full url of the current page
+export function goto_url(url) {
+	location.href = url;
 }
